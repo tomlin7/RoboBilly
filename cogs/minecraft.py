7df61e07228ddefeb4925f5238adcfd0789166bd
Billy <70792552+billyeatcookies@users.noreply.github.com>
2021-06-21 21:52:33 +0300

    Minecraft server info

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -1,21 +1,60 @@
 from discord.ext import commands
 from discord import Embed, Color
 
+from mcstatus import MinecraftServer
+
 
 class Minecraft(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
+    @commands.group(name="mc")
+    async def mc(self, ctx):
+        pass
     
     @commands.command(name="coord", aliases=["coordinates", "pos"])
     async def coord(self, ctx, x=0, y=0, z=0):
         await ctx.send(embed=Embed(title="", description=f"X: **{x}**, Y: **{y}**, Z: **{z}**", color=Color.dark_theme()))
         
-    @commands.command(name="ip", aliases=["serverip", "mcip"])
+    @mc.command(name="ip", aliases=["serverip", "mcip"])
     async def ip(self, ctx):
         embed = Embed(title="Basement Minecraft Servers", description="__**Info**__\nYour Minecraft version should match **1.17**.\nCracked accounts are allowed to join.", color=Color.dark_theme())
         embed.add_field(name="Server 1", value="**IP Address**:`billysbasement.aternos.me`\n**OP**:<@!621397007332016145>", inline=False)
         embed.add_field(name="Server 2", value="**IP Address**:`147.135.71.70:25592`\n**OP**:<@!517998886141558786>", inline=False)
+        await ctx.send(embed=embed)
+    
+    
+    
+    @mc.command(name="serverinfo", aliases=["sinfo", "mcinfo"])
+    async def serverinfo(self, ctx):
+        status1 = None
+        status2 = None
+        try:
+            server1 = MinecraftServer.lookup("billysbasement.aternos.me:54987")
+            status1 = server1.status()
+        except:
+            pass
+        try:
+            server2 = MinecraftServer.lookup("147.135.71.70:25592")
+            status2 = server2.status()
+        except:
+            pass
+        
+        embed = Embed(title="Basement Minecraft Servers Info", description="__**Info**__\nAsk the OP's for help if you can't join the server.\nPing them if the server is offline and you want to join, they will help you.", color=Color.dark_theme())
+        
+        if status1 is None and status2 is None:
+            embed.add_field(name="Servers are Offline", value="Sorry both of the servers are offline at the moment. Please Try asking server OP's.", inline=False)
+        
+        if status1 is not None:
+            embed.add_field(name="Server 1", value="**IP Address**: `billysbasement.aternos.me`\n**OP**: <@!621397007332016145>\n**Status**: ✔ Online\n**Players Online**: {0}\n**Minecraft Version: {1}\n**Latency**: {2}".format(status1.players.online, status1.version.name, status1.latency), inline=False)
+        else:
+            embed.add_field(name="Server 1", value="IP Address: billysbasement.aternos.me`\nOP: <@!621397007332016145>\nStatus: ✖ **Offline**\n".format(), inline=False)
+        
+        if status2 is not None:
+            embed.add_field(name="Server 2", value="**IP Address**: `147.135.71.70:25592`\n**OP**: <@!517998886141558786>\n**Status**: ✔ Online\n**Players Online**: {0}\n**Minecraft Version: {1}\n**Latency**: {2}".format(status2.players.online, status2.version.name, status2.latency), inline=False)
+        else:
+            embed.add_field(name="Server 2", value="IP Address: `147.135.71.70:25592`\nOP: <@!517998886141558786>\nStatus: ✖ **Offline**\n".format(), inline=False)
+        
         await ctx.send(embed=embed)
         
 def setup(bot):
