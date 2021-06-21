2d62b6e880fc2a4bc2eb3eb016e7ae8e6179297f
Billy <70792552+billyeatcookies@users.noreply.github.com>
2021-06-21 21:13:24 +0300

    Update minecraft.py

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -11,5 +11,12 @@ class Minecraft(commands.Cog):
     async def coord(self, ctx, x=0, y=0, z=0):
         await ctx.send(embed=Embed(title="", description=f"X: **{x}**, Y: **{y}**, Z: **{z}**", color=Color.dark_theme()))
         
+    @commands.command(name="ip", aliases=["serverip", "mcip"])
+    async def ip(self, ctx):
+        embed = Embed(title="Basement Minecraft Servers", description="__**Info**__\nYour Minecraft version should match **1.17**.\nCracked accounts are allowed to join.", color=Color.dark_theme())
+        embed.add_field(name="Server 1", value="**IP Address**:`billysbasement.aternos.me`\n**OP**:<@!621397007332016145>", inline=False)
+        embed.add_field(name="Server 2", value="**IP Address**:`147.135.71.70:25592`\n**OP**:<@!517998886141558786>", inline=False)
+        await ctx.send(embed=embed)
+        
 def setup(bot):
     bot.add_cog(Minecraft(bot))
