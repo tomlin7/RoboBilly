225c9e1e253c6b4d34c8571474e9d1f5904a03f7
Billy <70792552+billyeatcookies@users.noreply.github.com>
2021-09-02 01:37:11 -0500

    Update Bot.py

--- cogs/Bot.py
+++ cogs/Bot.py
@@ -1,33 +1,43 @@
-###### BOT MODULE ######
+"""
+Bot Module
+"""
 
 import discord
 import asyncio
 from discord.ext import commands
-from discord.ext.commands import has_permissions, MissingPermissions, BadArgument
+
 
 class Bot(commands.Cog):
+    """
+    Commands related to the bot.
+    """
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command()
+    @commands.command(aliases=["pong", "latency"])
     async def ping(self, ctx):
         async with ctx.channel.typing():
-    	    await asyncio.sleep(2)
     	    ping = str(self.bot.latency * 1000)
-    	    embed = discord.Embed(title="Pong!", description=f'Ping: {ping} ms', color = discord.Color.blue())
+    	    embed = discord.Embed(title="Pong!", description=f'Latency: {ping} ms', color = discord.Color.dark_theme())
         await ctx.send(embed=embed)
-        print("Event. ", ctx.author.name, " pinged! pong!")
+        print("Event: ", ctx.author.name, " checked latency.")
     
     @commands.command()
     async def prefix(self, ctx):
         async with ctx.channel.typing():
-            await asyncio.sleep(2)
             prefix = str(ctx.prefix)
-            embed = discord.Embed(title="Prefix", description=f"Bot's prefix is: `{prefix}`", color = discord.Color.blue())
+            embed = discord.Embed(title="Prefix", description=f"Bot's prefix is: `{prefix}`", color = discord.Color.dark_theme())
         await ctx.send(embed=embed)
-        print("Event. ", ctx.author.name, " checked Prefix")
+        print("Event: ", ctx.author.name, " checked prefix")
+    
+    @commands.command(aliases=["src", "sourcecode", "botsrc"])
+    async def source(self, ctx):
+        async with ctx.channel.typing():
+            prefix = str(ctx.prefix)
+            embed = discord.Embed(title="/src", description="[source code on github](https://github.com/basement-team/RoboBilly)", color = discord.Color.dark_theme())
+        await ctx.send(embed=embed)
+        print("Event: ", ctx.author.name, " checked bot src")
 
-#===================================== ADD COG ======================================#
-
+	
 def setup(bot):
 	bot.add_cog(Bot(bot))
