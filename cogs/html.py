3b5d59ff4fe970b2b61a3c2633c594d584c0a408
billyeatcookies <billydevbusiness@gmail.com>
2021-09-23 02:11:28 -0500

    fix imgkit, html module

--- cogs/html.py
+++ cogs/html.py
@@ -1,13 +1,15 @@
-import discord
+import discord, os
 from discord.ext import commands
 import imgkit
 
+# install wkhtmltopdf
+os.system("sudo apt install wkhtmltopdf")
 
 
 # config imgkit
 
-path_wkthmltoimage = "/app/wkhtmltoimage.exe"
-config = imgkit.config(wkhtmltoimage=path_wkthmltoimage)
+# path_wkthmltoimage = "/app/wkhtmltoimage.exe"
+# config = imgkit.config(wkhtmltoimage=path_wkthmltoimage)
 
 
 
@@ -19,25 +21,28 @@ class Html(commands.Cog):
     async def html(self, ctx):
         pass
     
-    @commands.command(name="html", aliases=["html_to_img"])
-    async def html(self, *, code):
+    @html.command(name="html_to_img", aliases=["html", "-h", "--html"])
+    async def html_to_img(self, *, code):
         with open("html_img.html", "w") as f:
             f.write(str(code))
 
         options = {
             'format': 'png'
         }
-        imgkit.from_file('html_img.html', 'out.png', options=options, config=config)
+        imgkit.from_file('html_img.html', 'out.png', options=options) #, config=config)
 
+        print("File created: out.png")
         await ctx.send(file=discord.File("out.png"))
 
-    @commands.command(name="url_to_img", aliases=["url_img"])
+    @html.command(name="url_to_img", aliases=["url", "-u", "--url"])
     async def url_to_img(self, url):
         options = {
             'format': 'png',
             'encoding': "UTF-8",
         }
-        imgkit.from_url(str(url), 'out.png', options=options, config=config)
+        imgkit.from_url(str(url), 'out.png', options=options) #, config=config)
+
+        print("File created: out.png")
         await ctx.send(file=discord.File("out.png"))
         
         
