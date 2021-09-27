976225a5ca91a664a010e61a408eff41ae1f1078
Billy <70792552+billyeatcookies@users.noreply.github.com>
2021-09-27 05:31:40 -0500

    Add ajian module >.<

---
+++ cogs/ajian.py
@@ -0,0 +1,35 @@
+from discord.ext import commands
+import random
+
+class Ajian(commands.Cog):
+    """
+    Commands related to the bot.
+    """
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.group
+    async def ajian(self, ctx):
+        """
+        >.<
+        """
+        await ctx.send("no u")
+        
+    @ajian.command()
+    async def bonk(self, ctx):
+        if random.choice([True, False]):
+            await ctx.send("We've successfully bonked ajian")
+        else:
+            await ctx.send("You got bonked by ajian, get got!")
+
+    @ajian.command()
+    async def hug(self, ctx):
+        await ctx.send("*ajian hugs back* 🤍")
+
+    @ajian.command()
+    async def owo(self, ctx):
+        await ctx.send("UwU")
+
+
+def setup(bot):
+    bot.add_cog(Ajian(bot))
