a7ee876e2440e77f93b8eee31f934114761e47b8
Billy <70792552+billyeatcookies@users.noreply.github.com>
2021-09-30 06:23:20 -0500

    'ClientSession.close' was never awaited

--- libs/httpsession.py
+++ libs/httpsession.py
@@ -12,5 +12,5 @@ http_session = ClientSession(
     connector=TCPConnector(resolver=AsyncResolver(), family=socket.AF_INET)
 )
 
-def close_session():
+async def close_session():
     http_session.close()
